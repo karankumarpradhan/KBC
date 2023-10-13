37cd1e0daf3adb2fe6d1430e7fb26d1abb6b2d51
karankumarpradhan <karanpradhan209>
2023-10-13 19:53:38 +0530

    initial commit

---
+++ kbc.py
@@ -0,0 +1,113 @@
+print("1st question : ")
+print("Question - Who is VIRAT KOHLI ?")
+
+print ("Option -    a = Footballer    ", "    b = Cricketer   ", "    c = Badmintonist    ", "    d = Chess master    ")
+
+
+
+print(input("Enter your answer : "))
+
+a = ("Footballer")
+b = ("Cricketer")
+c = ("Badmintonist")
+d = ("Chess master")
+
+
+ans = ("Cricketer")
+
+
+
+
+if ans == b:
+    print("You won 13$")
+   
+else:
+        print("wrong answer!")
+        
+
+'''print("2nd question : ")
+print("When was CHANDRAYAN 3 launch ?")
+
+print ("Option  -    a = 21th JULY    ", "    b = 7th JULY   ", "    c = 3rd AUGUST    ", "    d = 14th JULY    ")
+
+ans = ("14th JULY")
+print(input("Enter your answer : "))
+
+a = ("21th JULY")
+b = ("7th JULY")
+c = ("3rd AUGUST")
+d = ("14th JULY")
+
+if (ans is a):
+    print("sorry you lost the game BATTER LUCK NEXT TIME :)")
+   
+elif (ans is b):
+    print("sorry you lost the game BATTER LUCK NEXT TIME :)")
+
+elif (ans is c):
+    print("sorry you lost the game BATTER LUCK NEXT TIME :)")
+
+elif (ans is d):
+    print("You won 60$")
+    print("CONGRATULATION aap next round mein aa sakte ho ")
+else:
+    print("error 404")
+
+print("3rd question : ")
+print("Question - Father of INDIAN SPACE PROGRAM .")
+
+print ("Option -    a = APJ abdul kalam    ", "    b = Satish dhawan    ", "    c = Nambi narayana    ", "    d = Vikram sarabhai     ")
+
+
+ans = ("Vikram sarabhai")
+print(input("Enter your answer : "))
+
+a = ("APJ abdul kalam ")
+b = ("Satish dhawan")
+c = ("Nambi narayana")
+d = ("Vikram sarabhai")
+
+if (ans is a):
+    print("sorry you lost the game BATTER LUCK NEXT TIME :)")
+   
+elif (ans is b):
+    print("sorry you lost the game BATTER LUCK NEXT TIME :)")
+
+elif (ans is c):
+    print("sorry you lost the game BATTER LUCK NEXT TIME :)")
+
+elif (ans is d):
+    print("You won 120$")
+
+else:
+    print("error 404")
+
+
+print("4th question : ")
+print("Question - Chemical name of AMMONIA")
+
+print ("Option -    a =   NH3  ", "    b =  NH4   ", "    c =  NH   ", "    d =   NH2   ")
+
+
+ans = ("NH3")
+print(input("Enter your answer : "))
+
+a = ("NH3")
+b = ("NH4")
+c = ("NH")
+d = ("NH2")
+
+if (ans is a):
+    print("You won 241$")
+   
+elif (ans is b):
+    print("sorry you lost the game BATTER LUCK NEXT TIME :)")
+
+elif (ans is c):
+    print("sorry you lost the game BATTER LUCK NEXT TIME :)")
+
+elif (ans is d):
+    print("sorry you lost the game BATTER LUCK NEXT TIME :)")
+
+else:
+    print("error 404")'''
